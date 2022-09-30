4024fcedfd9edaa49d74a0516aed3ac0cf037083
Knoka1 <knoka1@hotmail.com>
2022-09-30 00:51:23 -0300

    refac: Separating the animated component from Home 3 and changing eyes

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { discount, rick_morty, eye } from "../../assets";
 import GetStarted from "../GetStarted";
+import RickAndMorty from "../RickAndMorty/RickAndMorty";
 import styles from "../../style";
-import heroStyles from "./heroStyles";
 
 const Hero = () => {
   return (
@@ -42,20 +42,11 @@ const Hero = () => {
           <span className="text-gradient">through delivery</span>.
         </p>
       </div>
+
       <div
         className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
       >
-        <img
-          id="anchor"
-          src={rick_morty}
-          className="w-[100%] h-[100%] relative z-[5]"
-        />
-        <div id="eyes">
-          <img src={eye} className={`top-[5px] ${heroStyles.eyes}`} />
-          <img src={eye} className={heroStyles.eyes} />
-          <img src={eye} className={heroStyles.eyes} />
-          <img src={eye} className={heroStyles.eyes} />
-        </div>
+        <RickAndMorty />
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient " />
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient " />
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient " />
