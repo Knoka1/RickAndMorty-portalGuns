d3c698b2d56a54547ad376e8fc143f3d5f5468d3
Knoka1 <knoka1@hotmail.com>
2022-09-30 01:00:46 -0300

    fix: Small cleanup on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import React from "react";
 
 import Home from "./views/Home";
-import Login from "./views/Login";
 import Dashboard from "./views/Dashboard";
-import Orders from "./views/Orders";
-import { Routes, Route } from "react-router-dom";
 
-import styles from "./style";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="bg-white w-full overflow-hidden">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
         <Route path="dashboard" element={<Dashboard />} />
       </Routes>
     </div>
